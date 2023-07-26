1e624eed298ff5bb251327e35fdcd4e5e139034b
sinchang <sinchangwen@gmail.com>
2023-07-27 00:51:32 +0800

    Update search icon

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@
 'use client'
 
 import { useDebounce } from "@uidotdev/usehooks"
+import { SearchIcon } from "lucide-react";
 import Link from "next/link"
 import React, { ChangeEvent } from "react"
 
@@ -18,7 +19,7 @@ export default function IndexPage() {
   };
 
   React.useEffect(() => {
-    const searchHN = async () => {
+    const searchPod = async () => {
       let results = [];
       setIsSearching(true);
       if (debouncedSearchTerm && searchTerm) {
@@ -31,16 +32,14 @@ export default function IndexPage() {
       setResults(results);
     };
 
-    searchHN();
+    searchPod();
   }, [debouncedSearchTerm, searchTerm]);
   return <Container className="mt-[20vh]">
     <form>
       <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
       <div className="relative">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-          </svg>
+          <SearchIcon className="text-gray-500 dark:text-gray-400" />
         </div>
         <input type="search" onChange={handleChange} id="default-search" name="search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Podcast..." required />
         <div className="absolute right-2.5 bottom-2.5">
@@ -58,8 +57,9 @@ export default function IndexPage() {
         </li> : null
       )}
     </ul>
-      : null}
-  </Container>
+      : <>{searchTerm ? <div className="border-neutral-200 shadow-lg border p-4 text-center">No results found.</div> : null}</>
+    }
+  </Container >
 
 }
 
